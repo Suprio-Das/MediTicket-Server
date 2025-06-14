714ffd9ab35b03d01aebeac859c94e0f7c103375
Suprio-Das <priyamd507@gmail.com>
2025-06-14 20:36:46 +0600

    verification route created and implemented

--- index.js
+++ index.js
@@ -106,6 +106,14 @@ async function run() {
                 const generatedTicket = await Tickets.findOne(newTicketQuery);
                 res.send(generatedTicket);
             }
+
+            // Verfication Route
+            app.get('/verfication', async (req, res) => {
+                const regNo = req.query.regno;
+                const query = { "regNo": regNo };
+                const result = await RegNo.findOne(query);
+                res.send(result);
+            })
         })
 
     } finally {
